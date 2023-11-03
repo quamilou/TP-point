c33d28fe1a99bc0dfdfc9482ead4bad4a423115e
quamilou <quamilougame@gmail.com>
2023-11-03 15:09:50 +0100

    UPDATED | TP COURS 4

--- src/Point.java
+++ src/Point.java
@@ -3,17 +3,24 @@ public class Point {
 	int x;
 	int y;
 	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
 	public void InitialiseToi(int XInit, int YInit) {
-	x = XInit;
-	y = YInit;
+		x = XInit;
+		y = YInit;
 	}
-	
+
 	public void DeplaceToi(int XDep, int YDep) {
-		x = x+XDep;
-		y = y+YDep;
+		x = x + XDep;
+		y = y + YDep;
 	}
-	
+
 	public void AfficheToi() {
-		System.out.println("Je suis un point de coordonnes " + x +" "+ y);
+		System.out.println("Je suis un point de coordonnes " + x + " " + y);
 	}
+
+	
 }
--- src/UtilPoint.java
+++ src/UtilPoint.java
@@ -2,12 +2,35 @@
 public class UtilPoint {
 
 	public static void main(String[] args) {
-		Point UnPoint;
-		UnPoint = new Point();
-		UnPoint.InitialiseToi(5, 5);
-		UnPoint.AfficheToi();
-		UnPoint.DeplaceToi(2, 1);
-		UnPoint.AfficheToi();
+		Point UnPoint[] = new Point[10];
+
+		// b
+		System.out.println("\nb)");
+		for (int i = 0; i < UnPoint.length; i++) {
+			UnPoint[i] = new Point(i + 1, i + 1);
+			UnPoint[i].AfficheToi();
+		}
+
+		// c)
+		System.out.println("\nc)");
+		for (int i = 0; i < UnPoint.length; i++) {
+			UnPoint[i].AfficheToi();
+		}
+
+		// d)
+		for (int i = 0; i < UnPoint.length; i++) {
+			UnPoint[i].InitialiseToi((i + 1) % 10 + 1, (i + 1) % 10 + 1);
+		}
+
+		// e)
+		System.out.println("\ne)");
+		for (Point point : UnPoint) {
+			point.AfficheToi();
+		}
+//		UnPoint.InitialiseToi(5, 5);
+//		UnPoint.AfficheToi();
+//		UnPoint.DeplaceToi(2, 1);
+//		UnPoint.AfficheToi();
 	}
 
 }
